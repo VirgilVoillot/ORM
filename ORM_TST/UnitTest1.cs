5a4d3525ad10d6553f14ca29d774beeb4669a01d
Virgil <Virgil@DESKTOP-C3B5FSJ>
2021-10-16 18:36:04 -0400

    Add project folder from VS Code

---
+++ ORM_TST/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ORM_TST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ UseORM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UseORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
